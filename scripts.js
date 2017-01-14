51e2ef4fe1dfcbd00bb3560ecb266ad05c09f30b
Florian Tischler <f.tischler@pmc-services.de>
2017-01-14 19:09:40 +0100

    Changes to be committed: 	new file:   index.html 	new file:   scripts.js

---
+++ scripts.js
@@ -0,0 +1,139 @@
+function Auto(marke, farbe) {
+    //Step 1:
+    this.marke = marke; //public
+    this.farbe = farbe; //public
+    this.geschwindigkeit = 0;   //public, Initialwert: 0
+    this.hupen = hupen; //Funktionszuweisung
+    this.beschleunigen = beschleunigen; //Funktionszuweisung
+    this.bremsen = bremsen; //Funktionszuweisung
+
+    //Step 2:
+    this.beschleunigenAsync = beschleunigenAsync;
+    this.bremsenAsync = bremsenAsync;
+    this.darfBeschleunigen = true;
+    this.darfBremsen = true;
+    this.verzoegerung = 2000; //Zeit in ms
+
+    //Step 1:
+    function hupen() {
+        console.log("Tuuuuuut");
+    }
+
+    function beschleunigen() {
+        this.geschwindigkeit++;
+    }
+
+    function bremsen() {
+        this.geschwindigkeit--;
+    }
+
+    //Step 2:
+    function beschleunigenAsync(successCallback, errorCallback) {
+        //Der Shit mit dem Scope von this:
+        var _this = this;
+
+        //Anonyme Function handler. Wird später setTimeout übergeben.
+        var handler = function() {
+          if(_this.darfBeschleunigen) {
+            _this.beschleunigen();
+            //Callback aufrufen:
+            successCallback(_this.geschwindigkeit);
+          }
+          else {
+            //Callback aufurfen:
+            errorCallback("Beschleunigen ist nicht erlaubt!");
+          }  
+        };
+
+        //setTimeout sorgt für einen zeitverzögerten Aufruf der Handler-Function (asynchron):
+        setTimeout(handler, this.verzoegerung);
+        console.log("Beschleunigungsvorgang eingeleitet");
+    }
+
+    function bremsenAsync(successCallback, errorCallback) {
+        //Der Shit mit dem Scope von this:
+        var _this = this;
+
+        //Anonyme Function handler. Wird später setTimeout übergeben.
+        var handler = function() {
+            if(_this.darfBremsen) {
+                _this.bremsen();
+                //Callback aufrufen:
+                successCallback(_this.geschwindigkeit);
+            }
+            else {
+                //Callback aufrufen:
+                errorCallback("Bremsen ist nicht erlaubt!");
+            }
+        }
+
+        //setTimeout sorgt für einen zeitverzögerten Aufruf der Handler-Function (asynchron):
+        setTimeout(handler, this.verzoegerung);
+        console.log("Bremsvorgang eingeleitet");
+    }
+}
+
+//Step 1:
+
+//Neues Auto-Objekt erstellen:
+var bmw = new Auto("BMW", "schwarz");
+
+//Hupen:
+bmw.hupen();
+
+//Marke und Farbe:
+console.log(bmw.marke);
+console.log(bmw.farbe);
+
+//Synchrones Fahren:
+console.log("Synchrones Fahren");
+console.log(bmw.geschwindigkeit);
+bmw.beschleunigen();
+console.log(bmw.geschwindigkeit);
+bmw.bremsen();
+console.log(bmw.geschwindigkeit);
+
+//Step 2:
+
+//Asynchrones Fahren:
+console.log("Asynchrones Fahren:");
+console.log(bmw.geschwindigkeit);
+
+bmw.beschleunigenAsync(function(geschwindigkeit) {
+    console.log("Der " + bmw.marke + " faehrt jetzt " + geschwindigkeit + " km/h");
+}, function(error) {
+    console.log(error);
+});
+
+console.log(bmw.geschwindigkeit);
+
+bmw.bremsenAsync(function(geschwindigkeit) {
+    console.log("Der " + bmw.marke + " faehrt jetzt " + geschwindigkeit + " km/h");
+}, function(error) {
+    console.log(error);
+});
+
+//Step 3:
+
+var audi = {
+    marke: "Audi",
+    farbe: "silber",
+    geschwindigkeit: 0,
+    bremsen: bmw.bremsen,
+    beschleunigen: bmw.beschleunigen,
+    hupen: function() {
+        console.log("Tutututututu ich bin 1 schwuler Audi");
+    }
+};
+
+console.log(audi.marke);
+console.log(audi.farbe);
+
+audi.hupen();
+
+console.log("Fahren");
+console.log(audi.geschwindigkeit);
+audi.beschleunigen();
+console.log(audi.geschwindigkeit);
+audi.bremsen();
+console.log(audi.geschwindigkeit);
